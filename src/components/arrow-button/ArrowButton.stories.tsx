c2036ac2558d271f5537b867b1abf6525cf9c40f
Ivan Pidko <vany2008@yandex.ru>
2024-08-15 02:46:33 +0300

    feat(arrowbutton): добавил ArrowButton
    BREAKING CHANGE: New feature

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,10 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton
+					isMenuOpen={false}
+					onClick={() => alert('Open/Close menu')}
+				/>
 			</>
 		);
 	},
--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,35 @@
 import arrow from 'src/images/arrow.svg';
-
+import clsx from 'clsx';
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type TArrowButtonProps = {
+	isMenuOpen: boolean;
+	onClick: OnClick;
+};
+
+export const ArrowButton = ({ isMenuOpen, onClick }: TArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			onClick={onClick}
+			className={clsx({
+				[styles.container]: true,
+				[styles.container_open]: isMenuOpen,
+			})}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx({
+					[styles.container]: true,
+					[styles.arrow_open]: isMenuOpen,
+				})}
+			/>
 		</div>
 	);
 };
